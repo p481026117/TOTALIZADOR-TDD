e76cdfc3bccf040d15d99916a9dcd24ff72c49f4
p48102611 <p481026110@gmail.com>
2021-10-01 14:30:41 -0400

    DESCUENTOS2

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -23,16 +23,50 @@ describe("Totalizador ", () => {
   it("deberia calcular para un estado por defecto", () => {
     expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
   });
+  it("deberia calcular el descuento", () => {
+    expect(calcularTotal(500, 2, "")).toEqual(970);
+  });
+  it("deberia calcular el descuento", () => {
+    expect(calcularTotal(550, 6, "")).toEqual(3135);
+  });
+  it("deberia calcular el descuento", () => {
+    expect(calcularTotal(110, 65, "")).toEqual(6649.5);
+  });
+  it("deberia calcular el descuento", () => {
+    expect(calcularTotal(110, 95, "")).toEqual(9405);
+  });
+  it("deberia calcular el descuento", () => {
+    expect(calcularTotal(200, 150, "")).toEqual(25500);
+  });
+
+  
+
 });
 
 function impuestoEstado(estado) {
   let impuestos = { UT: 0.0665, NV:0.0800, TX:0.0625, AL:0.0400, CA: 0.0825 ,"": 0 };
   return impuestos[estado];
 }
+function descuento(precioTotal){
+  var descuentoPrecio = 0;
+  if(precioTotal >= 30000)
+    descuentoPrecio = precioTotal*0.15;
+  else if(precioTotal >= 10000)
+    descuentoPrecio = precioTotal*0.1;
+  else if(precioTotal >= 7000)
+    descuentoPrecio = precioTotal*0.07;
+  else if(precioTotal >= 3000)
+    descuentoPrecio = precioTotal*0.05;
+  else if (precioTotal >= 1000)
+    descuentoPrecio = precioTotal*0.03;
+
+  return descuentoPrecio;
+
+}
 
 function calcularTotal(cantidad, precio, estado) {
   let subTotal = cantidad * precio;
   let impuesto = impuestoEstado(estado);
-  subTotal = subTotal + subTotal * impuesto;
+  subTotal = subTotal - descuento(subTotal) + subTotal * impuesto;
   return subTotal;
 }
